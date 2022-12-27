440c364fc4be35b650d5bac369b7cb73b7a2fad3
Fernanda Guerra <fercguerra06@gmail.com>
2022-12-27 20:09:07 -0300

    MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(
-    Container(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //MaterialApp é utilizado aepnas 1x
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
       child: Center(
-        child: Text(
-          'Flutterando',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 50.0,
-          ),
-        ),
+        child: Text('Flutterando'),
       ),
-    ),
-  );
+    );
+  }
 }
