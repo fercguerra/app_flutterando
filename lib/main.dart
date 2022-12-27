ede46ea05a998b440fecd012603294b342b9ad7f
Fernanda Guerra <fercguerra06@gmail.com>
2022-12-27 20:21:53 -0300

    componetização

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,6 @@
 import 'package:flutter/material.dart';
+import 'myapp.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    //MaterialApp é utilizado aepnas 1x
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
-        body: Center(
-          child: const Text('Flutterando'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('Clicou');
-          },
-        ));
-  }
 }
