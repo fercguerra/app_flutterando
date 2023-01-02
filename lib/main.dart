3b6464291af0c11f596b613fb7706e3559320714
Fernanda Guerra <fercguerra06@gmail.com>
2023-01-02 20:38:50 -0300

    Revisões

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,42 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
-import 'myapp.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    darkTheme: ThemeData(),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Curso Flutter'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(child: TextField()),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
